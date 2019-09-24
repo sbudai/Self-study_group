d4a44a99af5125ca9a46225bb651e42821ddde06
misrori <ormraat.pte@gmail.com>
2019-09-24 13:53:17 +0200

    comment added

--- atm_geocoding/atm_geocoding_20161104.R
+++ atm_geocoding/atm_geocoding_20161104.R
@@ -7,3 +7,5 @@ third_section <- adat[, ][4401:6800]
 third_section[, ':=' (lon = geocode(cimek)[[1]], lat = geocode(cimek)[[2]])]
 
 write.csv(third_section, 'atm_list_geocoded_third_section.csv')
+
+# great work
